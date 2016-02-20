f1700de24637b1b6b15268d3c113a2f4bc2856b1
Paul Crofts <paulcrofts@Pauls-MBP.Home>
2016-02-19 19:31:30 -0700

    Initial Sample Code

--- main.go
+++ main.go
@@ -3,13 +3,31 @@ package main
 import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/hybridgroup/gobot"
+	"github.com/hybridgroup/gobot/platforms/gpio"
+	"github.com/hybridgroup/gobot/platforms/raspi"
 )
 
 func main() {
 	log.Info("Starting PiHexGo")
 
-	gBot := gobot.NewGobot()
-	if gBot == nil {
+	gbot := gobot.NewGobot()
+	if gbot == nil {
 		log.Fatal("Unable to get Gobot")
 	}
+
+	pi := raspi.NewRaspiAdaptor("raspi")
+	pin := gpio.NewDirectPinDriver(pi, "pin", "18")
+
+	work := func() {
+		log.Info("Work")
+		pin.DigitalWrite(1)
+	}
+
+	robot := gobot.NewRobot("pihexgo",
+		[]gobot.Connection{pi},
+		[]gobot.Device{pin},
+		work,
+	)
+	gbot.AddRobot(robot)
+	gbot.Start()
 }
