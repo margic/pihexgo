fa8bcd3660e33099cddedbd776ec0cff5e5dddf8
Paul Crofts <paulcrofts@Pauls-MBP.Home>
2016-02-27 13:11:07 -0700

    Blinking test

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
@@ -10,6 +13,7 @@ import (
 )
 
 func main() {
+	log.Info("Starting PiHexGo")
 
 	viper.SetConfigName("config")
 	viper.AddConfigPath("/etc/pihex/")
@@ -25,26 +29,62 @@ func main() {
 		log.SetLevel(log.DebugLevel)
 	}
 
-	log.Info("Starting PiHexGo")
+	// handle close
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	log.Debug("Init GPIO")
+	go func() {
+		sig := <-sigs
+		log.Info(sig)
+		done <- true
+	}()
 
-	embd.InitGPIO()
-	defer embd.CloseGPIO()
-	embd.SetDirection(18, embd.Out)
+	Init()
+	defer Close()
+
+	go run()
+
+	// block until done
+	<-done
+	log.Debug("Shutting down.")
+}
+
+func run() {
+	pin, err := embd.NewDigitalPin(18)
+
+	if err != nil {
+		log.Fatal("Error getting pin", err)
+	}
+
+	pin.SetDirection(embd.Out)
 
 	ticker := time.NewTicker(time.Second * 1)
 	off := true
 	for range ticker.C {
 		if off {
-			embd.DigitalWrite(18, embd.High)
+			err = pin.Write(embd.High)
 			off = false
 			log.Debug("On")
 		} else {
-			embd.DigitalWrite(24, embd.Low)
+			err = pin.Write(embd.Low)
 			off = true
 			log.Debug("Off")
 		}
+		if err != nil {
+			log.Fatal("Error writing to pin", err)
+		}
 	}
+}
 
+// Init initialize the raspberry pi
+func Init() {
+	log.Info("Pi Time, initializing the Pi")
+	embd.InitGPIO()
+}
+
+// Close clean up resources on shutdown
+func Close() {
+	log.Info("Cleaning up")
+	defer embd.CloseGPIO()
 }
