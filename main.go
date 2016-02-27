6d6d6a7c00dd8f860104424fd105cd409c61f4d1
Paul Crofts <paulcrofts@Pauls-MBP.Home>
2016-02-27 11:59:12 -0700

    testing embd

--- main.go
+++ main.go
@@ -1,9 +1,50 @@
 package main
 
 import (
+	"time"
+
 	log "github.com/Sirupsen/logrus"
+	"github.com/kidoman/embd"
+	_ "github.com/kidoman/embd/host/rpi"
+	"github.com/spf13/viper"
 )
 
 func main() {
+
+	viper.SetConfigName("config")
+	viper.AddConfigPath("/etc/pihex/")
+	viper.AddConfigPath("$HOME/.pihex")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil { // Handle errors reading the config file
+		log.Fatal("Failed to read a config ", err)
+	}
+
+	debug := viper.GetBool("debug")
+	if debug {
+		log.SetLevel(log.DebugLevel)
+	}
+
 	log.Info("Starting PiHexGo")
+
+	log.Debug("Init GPIO")
+
+	embd.InitGPIO()
+	defer embd.CloseGPIO()
+	embd.SetDirection(18, embd.Out)
+
+	ticker := time.NewTicker(time.Second * 1)
+	off := true
+	for range ticker.C {
+		if off {
+			embd.DigitalWrite(18, embd.High)
+			off = false
+			log.Debug("On")
+		} else {
+			embd.DigitalWrite(24, embd.Low)
+			off = true
+			log.Debug("Off")
+		}
+	}
+
 }
